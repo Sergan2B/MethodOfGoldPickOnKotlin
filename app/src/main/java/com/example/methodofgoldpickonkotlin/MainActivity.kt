d77a12b1cbaf7e5d086e9003e6064795b0e641a7
Sergan2B <91610105+Sergan2B@users.noreply.github.com>
2022-06-17 21:32:14 +0600

    lola

--- app/src/main/java/com/example/methodofgoldpickonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/methodofgoldpickonkotlin/MainActivity.kt
@@ -10,15 +10,13 @@ import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var x1: Double = 0.0
+    var x1: Double = 0.0
+    var x2: Double = 0.0
+    var x11: Double = 0.0
+    var x22: Double = 0.0
     private var y1: Double = 0.0
     private var t1 = 0.0
     private var t2 = 0.0
-    private var goldenSection = GoldenSection()
-    private var g1: Double = 0.0
-    private var g2: Double = 0.0
-    private var j1: Double = 0.0
-    private var j2: Double = 0.0
     private lateinit var functionMath: String
 
     //   private val entries = ArrayList<Entry>() swe
@@ -30,154 +28,65 @@ class MainActivity : AppCompatActivity() {
         sss()
     }
 
-    /*
-        private fun swe() {
-            // Массив координат точек линии
-
-            entries.add(Entry(g1.toFloat(), j1.toFloat()))
-            entries.add(Entry(g2.toFloat(), j2.toFloat()))
-            entries.add(Entry(t1.toFloat(), t2.toFloat()))
-
-            /*Toast.makeText(this, j1.toString(), Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, j2.toString(), Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, g1.toString(), Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, g2.toString(), Toast.LENGTH_SHORT).show()*/
-            Toast.makeText(this, t1.toString(), Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, t2.toString(), Toast.LENGTH_SHORT).show()
-
-            // На основании массива точек создаем вторую линию с названием
-            val datasetSecond = LineDataSet(entries, "График золотого сечения")
-            // График будет зеленого цвета
-            datasetSecond.color = Color.GREEN
-            // График будет плавным
-            datasetSecond.mode = LineDataSet.Mode.CUBIC_BEZIER
-            // Линии графиков соберем в один массив
-            val dataSets: ArrayList<ILineDataSet?> = ArrayList()
-            dataSets.add(datasetSecond)
-            // Создадим переменную  данных для графика
-            val data = LineData(dataSets)
-            // Передадим данные для графика в сам график
-            binding.chart.data = data
-            // График будет анимироваться 0.5 секунды
-            binding.chart.animateY(500)
-            binding.chart.invalidate()
-        }
-    */
     @SuppressLint("SetTextI18n")
     private fun sss() {
         binding.btnX.setOnClickListener {
             if (binding.etCount.text?.isEmpty() == true) {
                 Toast.makeText(this, "Заполните текст", Toast.LENGTH_SHORT).show()
-            } else x1 = binding.etCount.text.toString().toInt().toDouble()
+            } else x1 = binding.etCount.text.toString().toDouble()
         }
         binding.btnY.setOnClickListener {
             if (binding.etCount.text?.isEmpty() == true) {
                 Toast.makeText(this, "Заполните текст", Toast.LENGTH_SHORT).show()
-            } else y1 = binding.etCount.text.toString().toInt().toDouble()
+            } else y1 = binding.etCount.text.toString().toDouble()
         }
         binding.btnEnterFunction.setOnClickListener {
             if (binding.etFunction.text?.isEmpty() == true) {
                 Toast.makeText(this, "Заполните текст", Toast.LENGTH_SHORT).show()
             } else {
                 functionMath = binding.etFunction.text.toString()
-                Toast.makeText(this@MainActivity, eval(functionMath).toString(), Toast.LENGTH_LONG)
-                    .show()
+                Toast.makeText(this, functionMath, Toast.LENGTH_SHORT).show()
+                // Toast.makeText(this, evalG1(functionMath).toString(), Toast.LENGTH_SHORT).show()
             }
         }
         binding.btnCoordinate.setOnClickListener {
-            t1 = (goldenSection.findMin(x1, y1, 0.001)) //Минимум
-            t2 = (goldenSection.findMax(x1, y1, 0.001)) //Максимум
+            //  t1 = (goldenSection.findMin(x1, y1, 0.001)) //Минимум
+
+            t1 = findMin(x1, y1, 0.001)
+            t2 = findMax(x1, y1, 0.001)
             binding.funMin.text = "$t1 Минимум нашей функции".trimMargin()
             binding.funMax.text = "$t2 Максимум нашей функции".trimMargin()
         }
     }
 
-    inner class GoldenSection {
-        private val phi = (1 + sqrt(5.0)) / 2 //Постоянная фи
-        fun funny(x: Int) {}
-        fun f(x: Double): Double {
-            //return (x + 1).pow(3.0) + 5 * x.pow(2.0) //Наша функция
-            //return (2 * x.pow(2.0) - 1) / (sqrt(x.pow(2.0)- 2))
-            return 0.0
+    fun findMin(a: Double, b: Double, e: Double): Double {
+        val PHI = (1 + sqrt(5.0)) / 2 //Постоянная фи
+        var a = a
+        var b = b
+        while (true) {
+            x1 = b - (b - a) / PHI
+            x2 = a + (b - a) / PHI
+            if (evalG1(functionMath) >= evalG2(functionMath)) a = x1 else b = x2
+            if (abs(b - a) < e) break
         }
-
-        fun findMin(a: Double, b: Double, e: Double): Double { //Нахождение минимума
-            var athis = a //Возвращяется в виде x1
-            var bthis = b //Возвращяется в виде y1
-            while (true) {
-                this@MainActivity.g1 = bthis - (bthis - athis) / phi
-                this@MainActivity.g2 = athis + (bthis - athis) / phi
-                if (f(g1) >= f(g2)) {
-                }//Если q1 больше или равен q2 выполняем athis равен q1
-                if (f(g1) >= f(g2)) {
-                    athis = g1
-                } else { //В другом случае bthis равен q2
-                    bthis = g2
-                }
-                if (abs(bthis - athis) < e) break
-            }
-            return (athis + bthis) / 2
-        }
-
-        fun findMax(a: Double, b: Double, e: Double): Double { //нахождение максимума
-            var athis = a //Возвращяется в виде x2
-            var bthis = b //Возвращяется в виде y2
-            while (true) {
-                this@MainActivity.j1 = bthis - (bthis - athis) / phi
-                this@MainActivity.j2 = athis + (bthis - athis) / phi
-                if (f(j1) <= f(j2)) {
-                    athis = j1
-                } else {
-                    bthis = j2
-                }
-                if (abs(bthis - athis) < e) break
-            }
-            return (athis + bthis) / 2
-        }
+        return (a + b) / 2
     }
 
-    private fun eval(str: String): Double {
-        val phi = (1 + sqrt(5.0)) / 2 //Постоянная фи
-        fun funny(x: Int) {}
-        fun f(x: Double): Double {
-            //return (x + 1).pow(3.0) + 5 * x.pow(2.0) //Наша функция
-            //return (2 * x.pow(2.0) - 1) / (sqrt(x.pow(2.0)- 2))
-            return 0.0
-        }
+    fun findMax(a: Double, b: Double, e: Double): Double {
+        val PHI = (1 + sqrt(5.0)) / 2 //Постоянная фи
+        var a = a
+        var b = b
 
-        fun findMin(a: Double, b: Double, e: Double): Double { //Нахождение минимума
-            var athis = a //Возвращяется в виде x1
-            var bthis = b //Возвращяется в виде y1
-            while (true) {
-                this@MainActivity.g1 = bthis - (bthis - athis) / phi
-                this@MainActivity.g2 = athis + (bthis - athis) / phi
-                if (f(g1) >= f(g2)) {
-                }//Если q1 больше или равен q2 выполняем athis равен q1
-                if (f(g1) >= f(g2)) {
-                    athis = g1
-                } else { //В другом случае bthis равен q2
-                    bthis = g2
-                }
-                if (abs(bthis - athis) < e) break
-            }
-            return (athis + bthis) / 2
+        while (true) {
+            x11 = b - (b - a) / PHI
+            x22 = a + (b - a) / PHI
+            if (evalJ1(functionMath) >= evalJ2(functionMath)) a = x1 else b = x2
+            if (Math.abs(b - a) < e) break
         }
+        return (a + b) / 2
+    }
 
-        fun findMax(a: Double, b: Double, e: Double): Double { //нахождение максимума
-            var athis = a //Возвращяется в виде x2
-            var bthis = b //Возвращяется в виде y2
-            while (true) {
-                this@MainActivity.j1 = bthis - (bthis - athis) / phi
-                this@MainActivity.j2 = athis + (bthis - athis) / phi
-                if (f(j1) <= f(j2)) {
-                    athis = j1
-                } else {
-                    bthis = j2
-                }
-                if (abs(bthis - athis) < e) break
-            }
-            return (athis + bthis) / 2
-        }
+    private fun evalG1(str: String): Double {
         return object : Any() {
             var pos = -1
             var ch = 0
@@ -231,14 +140,8 @@ class MainActivity : AppCompatActivity() {
                 if (eat('('.code)) { // parentheses
                     x = parseExpression()
                     if (!eat(')'.code)) throw RuntimeException("Missing ')'")
-                }
-                else if (ch == 'x'.code) {
-                    goldenSection.funny(ch)
-                    x = str.substring(startPos, pos).toDouble()
-                }
-                else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
+                } else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
                     while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) nextChar()
-
                     x = str.substring(startPos, pos).toDouble()
                 } else if (ch >= 'a'.code && ch <= 'z'.code) { // functions
                     while (ch >= 'a'.code && ch <= 'z'.code) nextChar()
@@ -250,6 +153,252 @@ class MainActivity : AppCompatActivity() {
                         x = parseFactor()
                     }
                     x = when (func) {
+                        "x" -> x1
+                        "sqrt" -> sqrt(x)
+                        "sin" -> sin(Math.toRadians(x))
+                        "cos" -> cos(Math.toRadians(x))
+                        "tan" -> tan(Math.toRadians(x))
+                        else -> throw RuntimeException("Unknown function: $func")
+                    }
+                } else {
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+                return x
+            }
+        }.parse()
+    }
+
+    private fun evalG2(str: String): Double {
+        return object : Any() {
+            var pos = -1
+            var ch = 0
+            fun nextChar() {
+                ch = if (++pos < str.length) {
+                    str[pos].code
+                } else -1
+            }
+
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.code) nextChar()
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.code)) x += parseTerm() // addition
+                    else if (eat('-'.code)) x -= parseTerm() // subtraction
+                    else return x
+                }
+            }
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'.code)) x *= parseFactor() // multiplication
+                    else if (eat('/'.code)) x /= parseFactor() // division
+                    else if (eat('^'.code)) x = x.pow(parseFactor())
+                    else return x
+                }
+            }
+
+            fun parseFactor(): Double {
+                if (eat('+'.code)) return +parseFactor() // unary plus
+                if (eat('-'.code)) return -parseFactor() // unary minus
+                var x: Double
+                val startPos = pos
+                if (eat('('.code)) { // parentheses
+                    x = parseExpression()
+                    if (!eat(')'.code)) throw RuntimeException("Missing ')'")
+                } else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
+                    while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) nextChar()
+                    x = str.substring(startPos, pos).toDouble()
+                } else if (ch >= 'a'.code && ch <= 'z'.code) { // functions
+                    while (ch >= 'a'.code && ch <= 'z'.code) nextChar()
+                    val func = str.substring(startPos, pos)
+                    if (eat('('.code)) {
+                        x = parseExpression()
+                        if (!eat(')'.code)) throw RuntimeException("Missing ')' after argument to $func")
+                    } else {
+                        x = parseFactor()
+                    }
+                    x = when (func) {
+                        "x" -> x2
+                        "sqrt" -> sqrt(x)
+                        "sin" -> sin(Math.toRadians(x))
+                        "cos" -> cos(Math.toRadians(x))
+                        "tan" -> tan(Math.toRadians(x))
+                        else -> throw RuntimeException("Unknown function: $func")
+                    }
+                } else {
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+                return x
+            }
+        }.parse()
+    }
+
+    private fun evalJ1(str: String): Double {
+        return object : Any() {
+            var pos = -1
+            var ch = 0
+            fun nextChar() {
+                ch = if (++pos < str.length) {
+                    str[pos].code
+                } else -1
+            }
+
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.code) nextChar()
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.code)) x += parseTerm() // addition
+                    else if (eat('-'.code)) x -= parseTerm() // subtraction
+                    else return x
+                }
+            }
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'.code)) x *= parseFactor() // multiplication
+                    else if (eat('/'.code)) x /= parseFactor() // division
+                    else if (eat('^'.code)) {
+                        x = x.pow(parseFactor())
+                    } else return x
+                }
+            }
+
+            fun parseFactor(): Double {
+                if (eat('+'.code)) return +parseFactor() // unary plus
+                if (eat('-'.code)) return -parseFactor() // unary minus
+                var x: Double
+                val startPos = pos
+                if (eat('('.code)) { // parentheses
+                    x = parseExpression()
+                    if (!eat(')'.code)) throw RuntimeException("Missing ')'")
+                } else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
+                    while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) nextChar()
+                    x = str.substring(startPos, pos).toDouble()
+                } else if (ch >= 'a'.code && ch <= 'z'.code) { // functions
+                    while (ch >= 'a'.code && ch <= 'z'.code) nextChar()
+                    val func = str.substring(startPos, pos)
+                    if (eat('('.code)) {
+                        x = parseExpression()
+                        if (!eat(')'.code)) throw RuntimeException("Missing ')' after argument to $func")
+                    } else {
+                        x = parseFactor()
+                    }
+                    x = when (func) {
+                        "x" -> x11
+                        "sqrt" -> sqrt(x)
+                        "sin" -> sin(Math.toRadians(x))
+                        "cos" -> cos(Math.toRadians(x))
+                        "tan" -> tan(Math.toRadians(x))
+                        else -> throw RuntimeException("Unknown function: $func")
+                    }
+                } else {
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+                return x
+            }
+        }.parse()
+    }
+
+    private fun evalJ2(str: String): Double {
+        return object : Any() {
+            var pos = -1
+            var ch = 0
+            fun nextChar() {
+                ch = if (++pos < str.length) {
+                    str[pos].code
+                } else -1
+            }
+
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.code) nextChar()
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.code)) x += parseTerm() // addition
+                    else if (eat('-'.code)) x -= parseTerm() // subtraction
+                    else return x
+                }
+            }
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'.code)) x *= parseFactor() // multiplication
+                    else if (eat('/'.code)) x /= parseFactor() // division
+                    else if (eat('^'.code)) {
+                        x = x.pow(parseFactor())
+                    } else return x
+                }
+            }
+
+            fun parseFactor(): Double {
+                if (eat('+'.code)) return +parseFactor() // unary plus
+                if (eat('-'.code)) return -parseFactor() // unary minus
+                var x: Double
+                val startPos = pos
+                if (eat('('.code)) { // parentheses
+                    x = parseExpression()
+                    if (!eat(')'.code)) throw RuntimeException("Missing ')'")
+                } else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
+                    while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) nextChar()
+                    x = str.substring(startPos, pos).toDouble()
+                } else if (ch >= 'a'.code && ch <= 'z'.code) { // functions
+                    while (ch >= 'a'.code && ch <= 'z'.code) nextChar()
+                    val func = str.substring(startPos, pos)
+                    if (eat('('.code)) {
+                        x = parseExpression()
+                        if (!eat(')'.code)) throw RuntimeException("Missing ')' after argument to $func")
+                    } else {
+                        x = parseFactor()
+                    }
+                    x = when (func) {
+                        "x" -> x22
                         "sqrt" -> sqrt(x)
                         "sin" -> sin(Math.toRadians(x))
                         "cos" -> cos(Math.toRadians(x))
